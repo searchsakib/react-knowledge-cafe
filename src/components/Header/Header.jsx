e85115b4ef28823e18d35b16c974e80e22ae7d52
searchsakib <searchsakib@gmail.com>
2023-09-12 16:14:01 +0600

    pictures added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import profile from '../../assets/images/New folder/profile.png';
+
 const Header = () => {
   return (
     <div>
       <h1 className="text-4xl font-semibold text-center m-7">Knowledge Cafe</h1>
+      <img src={profile} alt="" />
     </div>
   );
 };
