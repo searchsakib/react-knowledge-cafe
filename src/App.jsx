f148df283921b1b97ccfc54080d27b48b74b2bdb
searchsakib <searchsakib@gmail.com>
2023-09-12 15:58:59 +0600

    data fetched from blogs.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Header from './components/Header/Header';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-semibold text-center m-7">Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
