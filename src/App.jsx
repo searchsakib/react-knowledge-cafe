91780579571c6a2cc4d75cc4ae694e25245664e5
searchsakib <searchsakib@gmail.com>
2023-09-12 13:48:57 +0600

    added daisyui theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-4xl text-center m-5">Hello boys</h1>
+      <h1 className="text-primary text-4xl text-center m-5">Hello boys</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#aefcbf',
+
+          secondary: '#ffbadb',
+
+          accent: '#de5eed',
+
+          neutral: '#1a222d',
+
+          'base-100': '#31404e',
+
+          info: '#45bdde',
+
+          success: '#55d894',
+
+          warning: '#f9b858',
+
+          error: '#f44e67',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 };
