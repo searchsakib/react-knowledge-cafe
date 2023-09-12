b873f29eafe11382d16b1667f5f5fbd393e94ef4
searchsakib <searchsakib@gmail.com>
2023-09-12 13:56:50 +0600

    checking again

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1 className="text-primary text-4xl text-center m-5">Hello boys</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
